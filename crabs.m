37abf84da687f636afad525a7e9c62b4fc247433
MHickey <77748347+ThillySot@users.noreply.github.com>
2023-10-06 13:50:52 -0700

    added cmd and captain movement, made moveArm run through moveCapt

--- crabs.m
+++ crabs.m
@@ -1,4 +1,4 @@
-function crabs (moveArm)
+function crabs ()
 % Crabs is a kids computer game where a fisherman, called the captain,
 % hunts for a very clever and powerful crab.
 % Draw the game map and initialize map dimensions.
@@ -10,6 +10,7 @@ xCapt = 1200;
 yCapt = 1000;
 thetaCapt = -pi/2;
 sizeCapt = 50;
+moveArm = 'fals'
 
 % Draw the captain and initialize graphics handles
 %*********************************************************
@@ -18,5 +19,27 @@ sizeCapt = 50;
 %*******************************************************
 
 captainGraphics = drawCapt (xCapt, yCapt, thetaCapt, sizeCapt, moveArm);
+
+cmd = "null"; % initial command
+
+while ( cmd != "Q") % While not quit, read keyboard and respond
+
+cmd = kbhit(); % Read the keyboard.
+
+if( cmd == "w" || cmd == "a" || cmd == "d" || cmd == "e")
+%Captain has moved. Respond.
+% erase old captain
+
+for i=1:length( captainGraphics )
+set( captainGraphics(i), 'Visible', 'off' );
+
+endfor
+% move capt
+[xCapt, yCapt, thetaCapt, moveArm] = moveCapt(cmd, xCapt, yCapt, thetaCapt);
+
+% draw new capt
+ captainGraphics = drawCapt( xCapt, yCapt, thetaCapt, sizeCapt, moveArm);
+endif
+endwhile
 
 endfunction
