1a40382f623ba17a2e7e999ffc12ce805210e6cb
MHickey <77748347+ThillySot@users.noreply.github.com>
2023-10-20 13:45:39 -0700

    fixed tracking

--- crabTracking.m
+++ crabTracking.m
@@ -1,10 +1,15 @@
 
-function [xCrab, yCrab, thetaCrab] = crabTracking (xCapt, yCapt, xCrab, yCrab)
+function [xCrab, yCrab, thetaCrab] = crabTracking (xCapt, yCapt, xCrab, yCrab);
 
-crabStep = 10;
+crabStep = 30;
 
- if(xCrab != xCapt && yCrab != yCapt)
+ if(xCrab != xCapt && yCrab != yCapt && xCrab < xCapt)
     thetaCrab = atan((yCapt-yCrab)/(xCapt-xCrab));
+    xCrab = xCrab + crabStep * cos(thetaCrab);
+    yCrab = yCrab + crabStep * sin(thetaCrab);
+
+  else
+    thetaCrab = atan((yCapt-yCrab)/(xCapt-xCrab)) + pi;
     xCrab = xCrab + crabStep * cos(thetaCrab);
     yCrab = yCrab + crabStep * sin(thetaCrab);
 endif
