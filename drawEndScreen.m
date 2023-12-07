bd68a59ce8026452455c4ddbc33b0e3ff56168cc
Jushyb <101308152+Jushyb@users.noreply.github.com>
2023-12-06 19:27:27 -0800

    Updated Endscreen
    added working counter of how many crabs caught out of however many crabs left on screen.

--- drawEndScreen.m
+++ drawEndScreen.m
@@ -1,13 +1,22 @@
 function restart = drawEndScreen(img, numCrabs, crabsCaught)
+  
+  
+  
+  crabsCaughtStr = num2str(crabsCaught);
+  
 
+  s = ["You Caught ", crabsCaughtStr, " out of ", num2str(numCrabs)];
+
+
+  
   drawMap(img);
   hold on
 
-  textHandle1 = text(700,500,'AH! Youve Been NIPPED! You Caught', 'FontSize', 20, 'Color', 'red');
+  textHandle1 = text(1700,500,strcat(s), 'FontSize', 20, 'Color', 'red');
 
   pause(4)
   delete(textHandle1)
-  textHandle2 = text(700,500,'would you like to play again?', 'FontSize', 20, 'Color', 'red');
+  textHandle2 = text(1600,500,'would you like to play again?', 'FontSize', 20, 'Color', 'red');
 
 
     while(1)
